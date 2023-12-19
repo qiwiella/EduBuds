55bf080d80c1413461a2735a0e27918bfa53d9d1
Yaren Akpınar <yaren.akpinar006@gmail.com>
2023-12-19 10:18:34 +0300

    sugar pop
    şeker seçim sprites

---
+++ EduBuds1/Assets/Scripts/Sugar Pop/Cands.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cands : MonoBehaviour
+{
+    float x, y;
+    bool should_it_fall = true; //düþsün mü? evet
+    GameObject choice_tool;
+
+    void Start(){
+        //seçim tagýndaki objeyi bu ve al
+        choice_tool = GameObject.FindGameObjectWithTag("choice");
+    }
+
+    // herbir þekerin hangi konumda olduðunu tutmamýz gerekiyor
+    // düþme iþlemini kontrol etmemiz geekiyor
+    public void Enter_New_Location(float _x, float _y)
+    {
+        //start da girdiðimiz deðerle buradaki deðerlere eþit olacak
+        x = _x;
+        y = _y;
+    }
+
+
+    
+    void Update()
+    { 
+        if(should_it_fall)
+        {
+            //Eðer þekerimin konumum, hedef konumumdan 0.2f den küçükse 
+            //düþme learpünü kapatsýn.en son posizyonu hedef konuma eþitlesin.
+            if( transform.position.y-y<0.05f)
+            {
+                should_it_fall = false;
+                transform.position = new Vector3(x, y, 0);
+            }
+            // iki deðer arasýnda yavaþca düþsün
+            //hangi hýzda inecek
+            transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, 0), Time.deltaTime * 3f);
+        }
+    }
+
+    //Fareye týklandýðý zaman seçim aracý buraya gelsin
+    void OnMouseDown()
+    {
+        choice_tool.transform.position = transform.position;
+    }
+}
--- EduBuds1/Assets/Scripts/Sugar Pop/Sugar_maker.cs
+++ EduBuds1/Assets/Scripts/Sugar Pop/Sugar_maker.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class game2Sugarmaker : MonoBehaviour
+public class Sugarmaker : MonoBehaviour
 {
-    public GameObject[] Cands;
+    public GameObject[] pre_Cands;
     public int width, height;
+    //oyundaki þekerleri bu matrise kaydet, konumuna göre
+    public Cands[,] candies_in_the_game;
+
 
     // döngüye alarak kaç tane oluþturacaðýmýza unity inspector kýsmýndan vermek istedim.
     void Start(){
-        for( int x = 0; x < width; x++ )
+        candies_in_the_game = new Cands[width, height];
+        for ( int x = 0; x < width; x++)
         {
             for( int y = 0; y < height; y++)
             {
@@ -21,16 +25,22 @@ public class game2Sugarmaker : MonoBehaviour
     public void Creat_Cands(int x, int y) // konumu vermesini isteyerek þeker oluþtuacaðýz.
     {
         // rastgele þeker al, kordinat, bir rotasyon vermedik(0,0)
+        
         GameObject new_cands = GameObject.Instantiate(Create_Random_Candy(), new Vector2(x, y+10), Quaternion.identity);
+
         // Her þekerin, þeker scriptini al, yeni konum oluþtur(creat_new_location) yap.
-        new_cands.GetComponent<game2_Cands>().Enter_New_Location(x, y);
+
+        Cands cand = new_cands.GetComponent<Cands> ();
+        cand.Enter_New_Location(x, y);
+        candies_in_the_game[x, y] = cand;
+
         // ben y de 10 konumda duruyorum y dðerine gelmesi için yavaþ yavaþ düþmesi gerekecek
     }
 
     public GameObject Create_Random_Candy()   //hangi þekeri oluþturduysam onu belirteceðim
     {
-        int rand = Random.Range(0, Cands.Length); //min 0, en yüksek þeker uzunluðu
-        return Cands[rand]; // bunlarýn içinden rastgele bir sayý alacak
+        int rand = Random.Range(0, pre_Cands.Length); //min 0, en yüksek þeker uzunluðu
+        return pre_Cands[rand]; // bunlarýn içinden rastgele bir sayý alacak
     }
     
     
