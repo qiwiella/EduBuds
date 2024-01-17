67e318caeb7b59d5cc33c30a3335c1f2016a2ae7
Yaren Akpınar <yaren.akpinar006@gmail.com>
2024-01-17 11:23:53 +0300

    script açıklamaları

--- EduBuds1/Assets/Scripts/2048/TileState.cs
+++ EduBuds1/Assets/Scripts/2048/TileState.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 
+// ScriptableObject'ýn yaratýlabilir menüde görünebilmesi için gerekli özellikler
 [CreateAssetMenu(menuName = "Tile State")]
 public class TileState : ScriptableObject
 {
+    // Tile'ýn üzerindeki sayý
     public int number;
+
+    // Tile'ýn arkaplan rengi
     public Color backgroundColor;
+
+    // Tile'ýn metin rengi
     public Color textColor;
 }
